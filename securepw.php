0e7edc9d5cf9ea7e88bda125656692e47b9a794c
Ivan Petak <ivan.petak1@gmail.com>
2017-09-14 01:34:32 +0200

    Update securepw.php
    Receiving and returning JSON data.

--- securepw.php
+++ securepw.php
@@ -1,16 +1,36 @@
 <?php
 include "spoj.php";
 
-$digits=md5($_POST['pin']);
-
-$sql="SELECT * FROM tags WHERE password='$digits'";
-$res=mysqli_query($con,$sql);
-if (mysqli_num_rows($res)==1)
+$getAjax = file_get_contents("php://input");
+$decodeAjax = json_decode($getAjax);
+$token = $decodeAjax->pin;
+$encToken=md5($token);
+try
 {
-	echo "Access Granted";
+	$sql="SELECT * FROM tags WHERE password='$encToken'";
+	$prep=$dbcon->prepare($sql);
+	$prep->execute();
+	$res=$prep->rowCount();
+	if ($res==1)
+	{
+		$yes = new \stdClass();
+		$yes->access="ACCESS GRANTED";
+		$answerYes=json_encode($yes);
+		echo $answerYes;
+	}
+	else
+	{
+		$no = new \stdClass();
+		$no->access="ACCESS DENIED";
+		$answerNo=json_encode($no);
+		echo $answerNo;
+	}
 }
-else
+catch(PDOException $e)
 {
-	echo "Access Denied";
+	$getError=$e->getMessage();
+	$error=json_encode($getError);
+	echo $error;
 }
+$dbcon=NULL;
 ?>
